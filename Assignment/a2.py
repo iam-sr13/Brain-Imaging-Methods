cee25fb476ecdb1250045d54c5ac5cd2b5b97fd2
Shriraj Sawant <iam-sr13@users.noreply.github.com>
2021-02-27 01:06:37 +0530

    Added code for gaussian smoothing
    Added 6 different levels of Gaussian smoothing and plotted corresponding histograms.

--- Assignment/a2.py
+++ Assignment/a2.py
@@ -27,4 +27,30 @@ plt.xlabel('Intensity [0-256] | Bins = 10')
 plt.ylabel('No. of Pixels')
 plt.show() #display histogram
 
+#Gaussian Smoothening
+sigmas=[5, 10, 20, 30, 40, 50] #levels of gaussian smoothening
+
+for sigma in sigmas:
+  smooth_img = sp.ndimage.gaussian_filter(img, sigma)
+  
+  plt.imshow(smooth_img, cmap='Greys_r') #start plotting image
+  plt.title("Smoothened Brain Image with shape {}x{} & sigma = {}".format(smooth_img.shape[0], smooth_img.shape[1], sigma)) #add title and image dimensions
+  plt.show() #display image on screen
+
+  plt.hist(smooth_img.ravel(),256,[0,256]) #plot histogram
+  plt.title("Histogram for the Smoothened Brain Image")
+  plt.xlabel('Intensity [0-256] | Bins = 256 | Sigma = {}'.format(sigma))
+  plt.ylabel('No. of Pixels')
+  plt.show() #display histogram
+
+  plt.hist(smooth_img.ravel(),10,[0,256]) #plot histogram
+  plt.title("Histogram for the Smoothened Brain Image")
+  plt.xlabel('Intensity [0-256] | Bins = 10 | Sigma = {}'.format(sigma))
+  plt.ylabel('No. of Pixels')
+  plt.show() #display histogram
+  
+print("Program ran successfully!")
+
+
+
 
