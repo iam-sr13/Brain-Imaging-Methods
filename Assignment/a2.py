7a48e816d5944cdce73b593a60e6c4ebcb805ba3
Shriraj Sawant <iam-sr13@users.noreply.github.com>
2021-02-27 00:14:53 +0530

    Loaded image
    Added code for loading brain image in original format i.e. greyscale. And wrote prototype code for showing the image and its dimensions.

--- Assignment/a2.py
+++ Assignment/a2.py
@@ -8,5 +8,9 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
+img = plt.imread("Brain_og.jpg") #Read image
+plt.imshow(img) #start plotting image
+plt.title("Original Brain Image with shape {}x{}".format(img.shape)) #add title and image dimensions
+plt.show() #display image on screen
 
 
