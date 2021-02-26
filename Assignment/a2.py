ea0de8c1c0763de6f205581d49f1941daf5302fc
Shriraj Sawant <iam-sr13@users.noreply.github.com>
2021-02-27 00:33:02 +0530

    Add histogram
    Testing code for plotting image histogram

--- Assignment/a2.py
+++ Assignment/a2.py
@@ -13,4 +13,10 @@ plt.imshow(img, cmap='Greys_r') #start plotting image
 plt.title("Original Brain Image with shape {}x{}".format(img.shape[0], img.shape[1])) #add title and image dimensions
 plt.show() #display image on screen
 
+plt.hist(img, bins=10) #plot histogram
+plt.title("Histogram for the Original Brain Image")
+plt.xlabel('Intensity')
+plt.ylabel('No. of Pixels')
+plt.show() #display histogram
+
 
