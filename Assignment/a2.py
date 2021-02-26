049b3b7d047dafedc06df1d0a8673ccff4cd53cf
Shriraj Sawant <iam-sr13@users.noreply.github.com>
2021-02-27 00:39:22 +0530

    improved histogram code

--- Assignment/a2.py
+++ Assignment/a2.py
@@ -13,9 +13,9 @@ plt.imshow(img, cmap='Greys_r') #start plotting image
 plt.title("Original Brain Image with shape {}x{}".format(img.shape[0], img.shape[1])) #add title and image dimensions
 plt.show() #display image on screen
 
-plt.hist(img, bins=10) #plot histogram
+plt.hist(img.ravel(),256,[0,256]) #plot histogram
 plt.title("Histogram for the Original Brain Image")
-plt.xlabel('Intensity')
+plt.xlabel('Intensity [0-256]')
 plt.ylabel('No. of Pixels')
 plt.show() #display histogram
 
