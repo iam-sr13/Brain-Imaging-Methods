2684f891cd43b4de1e65a4fdddec7fe3127ac7fb
Shriraj Sawant <iam-sr13@users.noreply.github.com>
2021-04-03 09:12:16 +0530

    import

--- Assignment/a3.py
+++ Assignment/a3.py
@@ -1,1 +1,5 @@
+from psychopy import visual, core, logging, event  # import some libraries from PsychoPy, include logging for creating logfiles
+from datetime import datetime # import datetime library for timestamp
+from psychopy.hardware import keyboard # import keyboard library
+
 
