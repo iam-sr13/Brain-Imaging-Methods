399716ce3cfa46ac410d83eecb443eff79901452
Shriraj Sawant <iam-sr13@users.noreply.github.com>
2021-04-09 00:04:52 +0530

    add function for parsing instructions

--- Assignment/a3.py
+++ Assignment/a3.py
@@ -4,4 +4,21 @@ from psychopy import data, gui, visual, core, logging, event
 from datetime import datetime 
 from psychopy.hardware import keyboard 
 
+###############################################################################
+#Import file handling libraries
+import codecs
+import csv
+import os
+import re
+
+#Basic Functions for reading/writing files and folders
+def parse_instructions(input, START, END):
+    """Function for getting instructions into correct format"""
+        m = re.compile(r'%s(.*)%s' % (START, END), re.DOTALL | re.MULTILINE)
+        text = m.search(input).group(1)
+
+        return text
+
+
+
 
